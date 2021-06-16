bb3b56da79dc653bfd17289d0b4f06ca53967217
Martin Irha <irha.martin@gmail.com>
2021-06-16 11:58:47 +0200

    rename i/o model

--- lib/src/wizard.dart
+++ lib/src/wizard.dart
@@ -20,73 +20,71 @@ class CronWizard {
   List<String> days   = ['sun','mon','tue','wed','thu','fri','sat','sun'];
   static int idCounter = 0;
   final int id;
-  
+
   final RegExp _reCron = new RegExp("^((?:\\*|(?:[0-9\\-\\,]+))(?:/\\d+)?)\\s+((?:\\*|(?:[0-9\\-\\,]+))(?:/\\d+)?)\\s+((?:\\*|(?:[0-9\\-\\,]+))(?:/\\d+)?)\\s+((?:\\*|(?:[0-9\\-\\,]+))(?:/\\d+)?)\\s+((?:\\*|(?:[0-9\\-\\,]+))(?:/\\d+)?)\\s*\$");
-  
+
   CronWizard(this.model, this.form) :
     id = idCounter++
   {}
 
 
-  @Input("render_interval")
-  void set render_interval(String val) {
+  @Input("crontab")
+  void set crontab(String val) {
     interval = val;
-//    render(interval);
   }
 
-  final _valueStream = new StreamController<String>();
-  @Output()
-  String get render_interval => _valueStream.stream;
+  final _crontabChange = new StreamController<String>();
+  @Output("crontabChange")
+  String get crontabChange => _crontabChange.stream;
 
 
   String get interval => _interval;
   void set interval(String value) {
-//    print("get interval");
     render(value);
   }
-  
+
   bool get anyFieldInvalid {
     if(form == null)
       return false;
     ["minute","hour","day","month","dow"].forEach((_) { if(form != null) return true; });
     return false;
   }
-  
+
   String get value => _value;
   void set value(String value) {
     _value = value;
   }
-  
+
   String get minute => _minute;
   void set minute(String value) {
     _minute = value;
     updateModel();
   }
-  
+
   String get hour => _hour;
   void set hour(String value) {
     _hour = value;
     updateModel();
   }
-  
+
   String get day => _day;
   void set day(String value) {
     _day = value;
     updateModel();
   }
-  
+
   String get month => _month;
   void set month(String value) {
     _month = value;
     updateModel();
   }
-  
+
   String get dow => _dow;
   void set dow(String value) {
     _dow = value;
     updateModel();
   }
-  
+
   void render(dynamic newValue) {
     if(newValue is String && _reCron.matchAsPrefix(newValue) != null) {
       value = newValue;
@@ -103,11 +101,10 @@ class CronWizard {
       _interval = newValue;
       invalid = true;
     }
-//    print("interval:  $_interval");
   }
-  
+
   void updateModel() {
     interval = "$minute $hour $day $month $dow";
-    _valueStream.add(interval);
+    _crontabChange.add(interval);
   }
 }
